9aa4dce544b216b8dd8318cf73da015a970cf6c9
hosunglee222 <castlelee222@gmail.com>
2024-11-13 22:12:38 +0900

    :sparkles: feat: 추천 정보 DTO 컨버터 메서드 구현

--- src/main/java/com/gachon/data_analysis/dto/RecommendStockDTO.java
+++ src/main/java/com/gachon/data_analysis/dto/RecommendStockDTO.java
@@ -1,5 +1,7 @@
 package com.gachon.data_analysis.dto;
 
+import com.gachon.data_analysis.entity.Stock;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,4 +12,12 @@ public class RecommendStockDTO {
     private String ticker;
     private String name;
     private Double recommendScore;
+
+    public static RecommendStockDTO fromEntity(Stock stock) {
+        return RecommendStockDTO.builder()
+                .ticker(stock.getTicker())
+                .name(stock.getName())
+                .recommendScore(stock.getRecommendationScore())
+                .build();
+    }
 }
